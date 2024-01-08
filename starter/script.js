cc4bbad1e2caa36d4717d5275d916f0b09c99cd5
emeritolopes <emerito.lopes@gmail.com>
2024-01-08 20:32:45 +0000

    added api key and web

---
+++ starter/script.js
@@ -0,0 +1,6 @@
+// weather API from https://openweathermap.org/
+$(document).ready(function() {
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+    var apiKey = "a860ca7198ad937ab5110dfba8ead454";
+
+})
