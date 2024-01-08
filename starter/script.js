2667282e34ef17fbad41a51c293b9d9b9f674ae1
emeritolopes <emerito.lopes@gmail.com>
2024-01-08 21:24:12 +0000

    added checkWeather function and testing

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,17 @@
 // weather API from https://openweathermap.org/
 $(document).ready(function() {
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-    var apiKey = "a860ca7198ad937ab5110dfba8ead454";
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=london";
+    var apiKey = "4930782434657daf748cc931d47eb007";
+
+// testing if api works 
+    async function checkWeather() {
+        var response = await fetch(apiURL + `&appid=${apiKey}`);
+        var data = await response.json();
+
+        console.log(data);
+    }
+checkWeather();
+    
 
 })
+
