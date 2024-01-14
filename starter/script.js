213e5ae994f3ab00893ba40999c4ba750f718812
emeritolopes <emerito.lopes@gmail.com>
2024-01-14 13:31:34 +0000

    added function to get the current weather from api. API web and key added too in variables

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,8 @@
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?&appid="
+var apiKey = "4930782434657daf748cc931d47eb007";
+
 // weather API from https://openweathermap.org/
+
 $(document).ready(function() {
     var cityForm = $("#search-form")
     var cityInput = $("#search-input")
@@ -9,4 +13,14 @@ $(document).ready(function() {
         var city = cityInput.val()  
         currentWeather(city); 
     })
-}
+    // current weather
+    function currentWeather(city) {
+        fetch(apiURL + city + "&appid=" + apiKey)
+        .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        displayWeather(data, todayContainer)
+      })
+    }
